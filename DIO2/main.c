8c175d1c5944a9a92d97f2ca1500993ba05639ff
mahmoud <engactivemedo@gmail.com>
2016-02-02 20:36:56 +0200

    befor walking away

--- DIO2/main.c
+++ DIO2/main.c
@@ -7,9 +7,29 @@
 #include "types.h"
 #include "DIO_Interface.h"
 
-int main (void)
-{
+int main(void) {
+	DIO_voidInit();
+	DIO_u8WritePortDir(DIO_u8PORT2, 0xff);
+	DIO_u8WritePortVal(DIO_u8PORT2, 0xff);
+	while (1) {
 
+		DIO_u8WritePortVal(DIO_u8PORT2,0x00);
+
+		delay11();
+		DIO_u8WritePortVal(DIO_u8PORT2,0xff);
+		delay11();
+	}
 
 	return 0;
+}
+
+void delay11(void) {
+	volatile u64 z;
+
+	for (z = 1000000000; z > 0; z--) {
+		//for (i = 1000000000; i > 0; i--) {
+			__asm__("NOP");
+			__asm__("NOP");
+		//}
+	}
 }
