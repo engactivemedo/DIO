fc0a9529b5005bb2099d7c4aa485ffb073a49832
mahmoud <engactivemedo@gmail.com>
2016-02-02 23:24:50 +0200

    aworking cood but still need optimization

--- DIO2/main.c
+++ DIO2/main.c
@@ -6,30 +6,63 @@
  */
 #include "types.h"
 #include "DIO_Interface.h"
+static volatile u8 shape[5][4] = { { 0b00011000, 0b00100100, 0b01000010,0b10000001 },
+								   { 0b10000001, 0b01000010, 0b00100100,0b00011000 },
+								   { 0b10001000, 0b01000100, 0b00100010, 0b00010001},
+								   {0b00010001, 0b00100010, 0b00100100, 0b00011000 },
+								   { 0b00011000,0b00111100, 0b01111110, 0b11111111 } };
+
+int delay11(int v);
 
 int main(void) {
 	DIO_voidInit();
 	DIO_u8WritePortDir(DIO_u8PORT2, 0xff);
 	DIO_u8WritePortVal(DIO_u8PORT2, 0xff);
 	while (1) {
+		u8 i = 0, k = 0;
+		i=1;
+		for (k = 0; k < 8; k++)
+		{
+			DIO_u8WritePortVal(DIO_u8PORT2,i);
+			i<<=1;
+			delay11(12);
+		}
+		i=0x80;;
+		for (k = 0; k < 8; k++)
+				{
+					DIO_u8WritePortVal(DIO_u8PORT2,i);
+					i>>=1;
+					delay11(12);
+				}
 
-		DIO_u8WritePortVal(DIO_u8PORT2,0x00);
+		for (k = 0; k < 4; k++) {
+			for (i = 0; i < 5; i++) {
+				DIO_u8WritePortVal(DIO_u8PORT2, shape[k][i]);
+				delay11(12);
+			}
 
-		delay11();
-		DIO_u8WritePortVal(DIO_u8PORT2,0xff);
-		delay11();
+		}
+
+		for (k = 0; k < 4; k++) {
+					for (i = 0; i < 5; i++) {
+						DIO_u8WritePortVal(DIO_u8PORT2, ~shape[k][i]);
+						delay11(12);
+					}
+
+				}
 	}
 
 	return 0;
 }
 
-void delay11(void) {
-	volatile u64 z;
+int delay11(int v) {
+	u32 z;
 
-	for (z = 1000000000; z > 0; z--) {
+	for (z = 100000; z > 0; z--) {
 		//for (i = 1000000000; i > 0; i--) {
-			__asm__("NOP");
-			__asm__("NOP");
+		__asm__("NOP");
+		//__asm__("NOP");
 		//}
 	}
+	return 1;
 }
